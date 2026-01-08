1ce2f856a2dc65065f649eeac2bf58560480322a
Stephane Mouako <stephane.mouako@sioux.eu>
2026-01-08 19:52:29 +0100

    Remove time-related utility functions for UTC timestamp

--- app/utils/time.py
+++
@@ -1,8 +0,0 @@
-"""Time-related helpers for the application."""
-
-from datetime import UTC, datetime
-
-
-def utcnow() -> datetime:
-    """Return UTC timestamp."""
-    return datetime.now(UTC).replace(tzinfo=None)
