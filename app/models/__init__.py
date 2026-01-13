e51e07144a8261fe690b413f5df15b1905a9b417
Stephane Mouako <stephane.mouako@sioux.eu>
2026-01-13 18:06:46 +0100

    Add Payment model import to app models initialization

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,6 +2,7 @@ from .address import Address
 from .cart import Cart, CartItem
 from .category import Category
 from .order import Order, OrderItem
+from .payment import Payment
 from .product import Product
 from .user import User
 from .wishlist_item import WishlistItem
